8583f4d974f5f2d89794cbf719b553f024999cdb
itauge <itauge@gmail.com>
2021-06-12 20:40:08 +0800

    test all @config

--- src/main/java/com/fung/boot/controller/HelloController.java
+++ src/main/java/com/fung/boot/controller/HelloController.java
@@ -1,12 +1,15 @@
 package com.fung.boot.controller;
 
 import com.fung.boot.bean.Car;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
@@ -19,8 +22,10 @@ public class HelloController {
     }
 
     @RequestMapping("/hello")
-    public String handle01(){
-        return "Hello,SpringBoot2";
+    public String handle01(@RequestParam("name") String name){
+
+        log.info("請求進來了");
+        return "Hello, " + name;
     }
 
 }
